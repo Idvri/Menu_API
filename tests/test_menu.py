2137e741d33082857196624df61246eb2d59a026
Nodar <nodar.panchenko@gmail.com>
2024-02-01 16:42:44 +0400

    Обновил тесты меню с аналгом Django reverse() для FastAPI - url_path_for

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -10,8 +10,8 @@ from main import app
 async def test_get_menus():
     """Тест по получению списка меню."""
 
-    async with AsyncClient(app=app, base_url='http://localhost:8000/api/v1/menus', follow_redirects=True) as ac:
-        response = await ac.get(url='/')
+    async with AsyncClient(app=app, base_url='http://localhost:8000', follow_redirects=True) as ac:
+        response = await ac.get(url=app.url_path_for('get_menus'))
         assert response.status_code == 200
 
         if not response.json():
@@ -30,14 +30,14 @@ async def test_get_menus():
 async def test_create_menu():
     """Тест по созданию меню."""
 
-    async with AsyncClient(app=app, base_url='http://localhost:8000/api/v1/menus', follow_redirects=True) as ac:
+    async with AsyncClient(app=app, base_url='http://localhost:8000', follow_redirects=True) as ac:
         # Тестирую положительный ответ.
         data = {
             'id': '7f59f0a0-db4a-4b8f-a832-f63796f448b4',
             'title': 'string',
             'description': 'string'
         }
-        response = await ac.post(url='/', content=json.dumps(data))
+        response = await ac.post(url=app.url_path_for('create_menu'), content=json.dumps(data))
         assert response.status_code == 201
         assert response.json() == {
             'id': '7f59f0a0-db4a-4b8f-a832-f63796f448b4',
@@ -48,7 +48,7 @@ async def test_create_menu():
         data = {
             'description': 'string'
         }
-        response = await ac.post(url='/', content=json.dumps(data))
+        response = await ac.post(url=app.url_path_for('create_menu'), content=json.dumps(data))
         assert response.status_code == 422
         assert response.json() == {
             'detail': [
@@ -72,17 +72,17 @@ async def test_create_menu():
 async def test_get_menu():
     """Тест по получению определенного меню."""
 
-    async with AsyncClient(app=app, base_url='http://localhost:8000/api/v1/menus', follow_redirects=True) as ac:
+    async with AsyncClient(app=app, base_url='http://localhost:8000', follow_redirects=True) as ac:
         # Тестирую положительный ответ.
         data = {
             'id': '7f59f0a0-db4a-4b8f-a832-f63796f448b5',
             'title': 'test title',
             'description': 'test description'
         }
-        await ac.post(url='/', content=json.dumps(data))
+        await ac.post(url=app.url_path_for('create_menu'), content=json.dumps(data))
 
         response = await ac.get(
-            url='/7f59f0a0-db4a-4b8f-a832-f63796f448b5',
+            url=app.url_path_for('get_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b5')
         )
         assert response.status_code == 200
         assert response.json() == {
@@ -94,7 +94,7 @@ async def test_get_menu():
         }
         # Тестирую 1 исключение.
         response = await ac.get(
-            url='/7f59f0a0-db4a-4b8f-a832-f63796f448b8',
+            url=app.url_path_for('get_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b8')
         )
         assert response.status_code == 404
         assert response.json() == {
@@ -102,7 +102,7 @@ async def test_get_menu():
         }
         # Тестирую 2 исключение.
         response = await ac.get(
-            url='/7f59f0a0-db4a-4b'
+            url=app.url_path_for('get_menu', target_menu_id='7f59f0a0-db4a-4b')
         )
         assert response.status_code == 422
         assert response.json() == {
@@ -128,13 +128,16 @@ async def test_get_menu():
 async def test_update_menu():
     """Тест по изменению определенного меню."""
 
-    async with AsyncClient(app=app, base_url='http://localhost:8000/api/v1/menus', follow_redirects=True) as ac:
+    async with AsyncClient(app=app, base_url='http://localhost:8000', follow_redirects=True) as ac:
         # Тестирую положительный ответ.
         data = {
             'title': 'test patched title',
             'description': 'test patched description'
         }
-        response = await ac.patch(url='/7f59f0a0-db4a-4b8f-a832-f63796f448b5', content=json.dumps(data))
+        response = await ac.patch(
+            url=app.url_path_for('update_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b5'),
+            content=json.dumps(data)
+        )
         assert response.status_code == 200
         assert response.json() == {
             'id': '7f59f0a0-db4a-4b8f-a832-f63796f448b5',
@@ -142,13 +145,19 @@ async def test_update_menu():
             'description': 'test patched description'
         }
         # Тестирую 1 исключение.
-        response = await ac.patch(url='/7f59f0a0-db4a-4b8f-a832-f63796f448b8', content=json.dumps(data))
+        response = await ac.patch(
+            url=app.url_path_for('update_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b8'),
+            content=json.dumps(data)
+        )
         assert response.status_code == 404
         assert response.json() == {
             'detail': 'menu not found'
         }
         # Тестирую 2 исключение.
-        response = await ac.patch(url='/7f59f0a0-db4a-4b', content=json.dumps(data))
+        response = await ac.patch(
+            url=app.url_path_for('update_menu', target_menu_id='7f59f0a0-db4a-4b'),
+            content=json.dumps(data)
+        )
         assert response.status_code == 422
         assert response.json() == {
             'detail': [
@@ -173,14 +182,16 @@ async def test_update_menu():
 async def test_delete_menu():
     """Тест по удалению определенного меню."""
 
-    async with AsyncClient(app=app, base_url='http://localhost:8000/api/v1/menus', follow_redirects=True) as ac:
+    async with AsyncClient(app=app, base_url='http://localhost:8000', follow_redirects=True) as ac:
         # Тестирую положительный ответ.
-        response = await ac.delete(url='/7f59f0a0-db4a-4b8f-a832-f63796f448b5')
+        response = await ac.delete(
+            url=app.url_path_for('delete_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b5')
+        )
         assert response.status_code == 200
         assert response.json() == {'message': 'Success.'}
         # Тестирую 1 исключение.
         response = await ac.delete(
-            url='/7f59f0a0-db4a-4b8f-a832-f63796f448b8',
+            url=app.url_path_for('update_menu', target_menu_id='7f59f0a0-db4a-4b8f-a832-f63796f448b8')
         )
         assert response.status_code == 404
         assert response.json() == {
@@ -188,7 +199,7 @@ async def test_delete_menu():
         }
         # Тестирую 2 исключение.
         response = await ac.delete(
-            url='/7f59f0a0-db4a-4b'
+            url=app.url_path_for('delete_menu', target_menu_id='7f59f0a0-db4a-4b')
         )
         assert response.status_code == 422
         assert response.json() == {
